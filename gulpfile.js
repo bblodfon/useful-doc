08128cca18ec1a610af4f2f2d0b7b412520cf15d
john <bblodfon@gmail.com>
2019-05-28 00:31:30 +0200

    updated gulpfile.js and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,37 @@ const gulp = require('gulp');
 const del = require('del');
 const exec = require('child_process').exec;
 const gulpLoadPlugins = require('gulp-load-plugins');
-
+const argv = require('yargs').argv;
 
 const $ = gulpLoadPlugins();
 
+gulp.task('git_add', function (callback) {
+  console.log('adding...');
+  exec('git add .', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback();
+  });
+});
+
+gulp.task('git_commit', function(callback) {
+  console.log('commiting...');
+  var cmd = 'git commit -m ' + '"' + argv.m + '"';
+  exec(cmd, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback();
+  });
+});
+
+gulp.task('git_push', function (callback) {
+  console.log('pushing...');
+  exec('git push', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback();
+  });
+});
 
 gulp.task('clean_docs', function () {
   return del(['docs/**', 'docs/.*', '!docs'], {
@@ -36,6 +63,6 @@ gulp.task('clean_book', function () {
 });
 
 gulp.task('default', gulp.series('clean_docs', 'build_book', 'copy_html_content',
-	  'clean_book', function (done) {
+  'clean_book', 'git_add', 'git_commit', 'git_push', function (done) {
   done();
 }));
